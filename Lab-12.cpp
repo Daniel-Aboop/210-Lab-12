0400fe742051b70f64c121a947fd65eade061fda
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-09-17 15:16:11 -0700

    3:16 pm

--- Lab-12.cpp
+++ Lab-12.cpp
@@ -13,7 +13,8 @@ void printList(const array<string,Size>&list){
     cout<<"Printing List:"<<endl;
 
     for(int i=0;i<Size;i++){
-        cout<<list[i]<<endl;
+        
+        cout<<i<<"#: "<<list[i]<<", ";
     }
     cout<<endl;
     
@@ -28,7 +29,7 @@ int main(){
     string response;
     while(true){
         cout<<"Welcome to the Grocery List, Enter F to enter a file path or for Manual Input Enter M"<<endl;
-        cout<<"Other functions enter L to list the Grocery List, Enter X to exit"<<endl;
+        cout<<"Other functions enter L to list the Grocery List,enter E to empty the list, Enter X to exit"<<endl;
         getline(cin,response);
         if(response=="F"){
             cout<<"Please enter file path"<<endl;
@@ -42,34 +43,31 @@ int main(){
                 cout<<"Entering data now...."<<endl;
                 string line;
                 int z=0;
-                while(getline(file,line)){
+                while(getline(file,line)&&z<=GroceryList.size()){
                 GroceryList[z]=line;
                 z++;
                 }
-                
-
             }
         }
         if(response=="M"){
+            string hold;
             for(int i=0;i<Size;i++){
+                cout<<"Enter Item for #"<<i;
+                getline(cin,hold);
 
-            }
-            
+
+            }   
         }
         if(response=="L"){
-            if(GroceryList.empty()!=0){
-                printList(GroceryList);
-            }
-            else{
-                
-                cout<<endl<<"Grocery List is empty please fill data!"<<endl<<endl;
-            }
+         printList(GroceryList);
+        }
+        if(response=="E"){
+            GroceryList.fill("");
+            cout<<"GroceryList set to blank"<<endl;
         }
         if(response=="X"){
             break;
         }
-
     }
-
     return 1;
 }
