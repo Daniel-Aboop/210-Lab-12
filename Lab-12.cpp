55c93852fbf4b74544e5841e113174992713169e
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-09-17 14:34:46 -0700

    2:34 pm

--- Lab-12.cpp
+++ Lab-12.cpp
@@ -3,16 +3,28 @@
 
 #include <iostream>
 #include <array>
+#include <fstream>
+#include <string>
 
 using namespace std;
 const int Size=30;
 
+void printList(const array<string,Size>&list){
+
+    
+}
+
+
 int main(){
     array<string, Size> GroceryList;
-    cout<<"Welcome to the Grocery List, Please enter a file path or manual input"<<endl;
+    string filepath;
+    
+    cout<<"Welcome to the Grocery List, Enter F to enter a file path or for Manual Input Enter M"<<endl;
+    cout<<"Other functions enter L to list the Grocery List, Enter X to exit"<<endl;
+    string response;
     while(true){
-
-
+        
+        getline(cin,response);
 
 
     }
