d66c65f885bf755b50e5bdd379baf6c4eda2bc0e
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-09-17 14:24:46 -0700

    2:24 pm

---
+++ Lab-12.cpp
@@ -0,0 +1,21 @@
+// COMSC-210 | Lab 12 | Daniel Santisteban
+// IDE used: VS Code
+
+#include <iostream>
+#include <array>
+
+using namespace std;
+const int Size=30;
+
+int main(){
+    array<string, Size> GroceryList;
+    cout<<"Welcome to the Grocery List, Please enter a file path or manual input"<<endl;
+    while(true){
+
+
+
+
+    }
+
+    return 1;
+}
